be2e35375e79324b70df6377ac7c4f1d99ab01bc
David McClure <davidwilliammcclure@gmail.com>
2015-05-03 20:20:47 -0700

    Stubbing in a basic Text class.

---
+++ toponyms/text.py
@@ -0,0 +1,41 @@
+
+
+class Text:
+
+
+    @classmethod
+    def from_file(cls, path):
+
+        """
+        Create a text from a file.
+
+        Args:
+            path (str): The file path.
+        """
+
+        with open(path, 'r', errors='replace') as f:
+            return cls(f.read())
+
+
+    def __init__(self, text):
+
+        """
+        Store the raw text.
+
+        Args:
+            text (str): The text string.
+        """
+
+        self.text = text
+
+
+    @property
+    def es_doc(self):
+
+        """
+        Provide a document for Elasticsearch.
+
+        Returns: dict
+        """
+
+        pass
