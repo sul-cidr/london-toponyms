66cc74d940646e3ebc68b8f25b80b5a321283669
David McClure <davidwilliammcclure@gmail.com>
2015-07-22 12:15:34 -0700

    Wires up RQ dashboard and basic query job.

---
+++ london/jobs.py
@@ -0,0 +1,38 @@
+
+
+from london.config import config
+
+
+def query(toponym, size=10000, frag_size=5000):
+
+    """
+    Query the paragraph index for a toponym.
+
+    Args:
+        toponym (str)
+    """
+
+    results = config.es.search('london', 'text', body={
+        'size': size,
+        'fields': [],
+        'query': {
+            'match_phrase': {
+                'text': {
+                    'query': toponym,
+                    'slop': 3
+                }
+            }
+        },
+        'highlight': {
+            'fields': {
+                'text': {
+                    'fragment_size': frag_size,
+                    'number_of_fragments': 1
+                }
+            }
+        }
+    })
+
+    for hit in results['hits']['hits']:
+        #snippet = hit['highlight']['text'][0]
+        print(hit)
